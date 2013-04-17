edfd1c210328edeecae716a8a8b668c47f99c88e
Seungkyu Yoon <skydx1@snuev.com>
2013-04-17 19:57:41 +0900

    First.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fanding::Application.config.secret_token = 'cc0756793ff6a204adb0f0a2fe212d48b3c00957a467e57ab3f5fcbfd2630167f0554b89bc7c244bc55c67a0b0c722a959706d16a0235a50938abadd03e3febf'
